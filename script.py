6ee635ce979970546633128295b44cb6da8bdfc4
daniel <kuningram@gmail.com>
2020-11-21 14:28:44 +0300

    log added

--- script.py
+++ script.py
@@ -2,14 +2,33 @@ import json
 import jsonschema
 from jsonschema import validate, Draft6Validator, Draft7Validator
 import glob
+import logging
+
+logging.basicConfig(filename="sample.log", level=logging.INFO)
+
+
+def validate_schemas():
+    path = 'schema/*.schema'   
+    files = glob.glob(path)
+    for filename in files: 
+        try:
+            with open(filename) as data: 
+                jsonschema = json.load(data)
+                Draft7Validator.check_schema(jsonschema)
+                print(filename + ' scheme validation successful')
+        except jsonschema.exceptions.ValidationError as err:
+            print(err.message)
+            return False
+        except IOError as exc:
+            if exc.errno != errno.EISDIR: 
+                raise
+    return True
 
 
 def validate_cmarker_created(jsonData):
     try:
         with open('schema/cmarker_created.schema') as schema: 
             jsonSchema = json.load(schema)
-            #validate(instance=jsonData, schema=jsonSchema)
-            #Draft6Validator(jsonSchema).validate(jsonData)
             Draft7Validator(jsonSchema).validate(jsonData)
     except jsonschema.exceptions.ValidationError as err:
         print(err.message)
@@ -20,7 +39,6 @@ def validate_label_selected(jsonData):
     try:
         with open('schema/label_selected.schema') as schema: 
             jsonSchema = json.load(schema)
-            #validate(instance=jsonData, schema=jsonSchema)
             Draft7Validator(jsonSchema).validate(jsonData)
     except jsonschema.exceptions.ValidationError as err:
         print(err.message)
@@ -31,7 +49,6 @@ def validate_sleep_created(jsonData):
     try:
         with open('schema/sleep_created.schema') as schema: 
             jsonSchema = json.load(schema)
-            #validate(instance=jsonData, schema=jsonSchema)
             Draft7Validator(jsonSchema).validate(jsonData)
     except jsonschema.exceptions.ValidationError as err:
         print(err.message)
@@ -42,7 +59,6 @@ def validate_workout_created(jsonData):
     try:
         with open('schema/workout_created.schema') as schema: 
             jsonSchema = json.load(schema)
-            #validate(jsonData, jsonSchema)
             Draft7Validator(jsonSchema).validate(jsonData)
     except jsonschema.exceptions.ValidationError as err:
         print(err.message)
@@ -50,6 +66,17 @@ def validate_workout_created(jsonData):
     return True
 
 if __name__ == '__main__':
+    # validating schemas
+    print('Check if schemas are valid.........')
+    if validate_schemas():
+        print('...all schemas are valid')
+        logging.info("Given schemas are valid")
+    else: 
+        print('...schemas invalid')
+        logging.error("Given schemas are invalid")
+
+    
+    # validating json with schemas    
     path = 'event/*.json'   
     files = glob.glob(path)
     for filename in files: 
